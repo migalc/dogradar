1a4c120c1909e23d37b1ba110d58ed04aaa553dc
Miguel Alcântara <miguel.alcantara@albelli.com>
2025-03-23 12:55:01 +0100

    Created project and workspace

---
+++ App/DogRadar/DogRadar/DogRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogRadarApp.swift
+//  DogRadar
+//
+//  Created by Miguel Alcantara on 23/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DogRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/DogRadar/DogRadarTests/DogRadarTests.swift
@@ -0,0 +1,17 @@
+//
+//  DogRadarTests.swift
+//  DogRadarTests
+//
+//  Created by Miguel Alcantara on 23/03/2025.
+//
+
+import Testing
+@testable import DogRadar
+
+struct DogRadarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
